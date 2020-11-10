7991107d5242208d54e6eed659bd9c0f0db67862
consoleLogIt <67229376+consoleLogIt@users.noreply.github.com>
2020-11-10 08:02:54 +0530

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import {LandingPage} from './Components'
 function App(props) {
     return (
         <div>
-        App
+        <LandingPage />
         </div>
     );
 }
 
-export default App;
+export default App; 
